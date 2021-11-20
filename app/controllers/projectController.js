23c5784edfc3617d6fb0ee671384e5ef1660dc3e
Emanuel Gonzales Rebatta <emgore.1410@gmail.com>
2021-11-19 19:09:48 -0500

    feat: Register project integration

--- app/controllers/projectController.js
+++ app/controllers/projectController.js
@@ -19,3 +19,23 @@ exports.getFullList = function (req, res) {
         }
     })
 }
+
+exports.save = function (req, res) {
+    ProjectService.save(req.body).then(function (result) {
+
+        if (result) {
+            return res.status(200).send({
+                data: result,
+                message: 'Project with id ' + result.insertId + ' created successfully',
+                idPosition: result.insertId
+            })
+        }
+    }, function (error) {
+        if (error) {
+            return res.status(401).send({
+                code: error.codeMessage,
+                message: error.message
+            })
+        }
+    })
+}
--- app/routes/projectRoutes.js
+++ app/routes/projectRoutes.js
@@ -12,4 +12,7 @@ const PROJECT_URL = endpoints.PROJECT_URL
 //GET FULL LIST
 router.get(PROJECT_URL.OPERATIONS.LIST, ProjectController.getFullList);
 
+//POST SAVE
+router.post(PROJECT_URL.OPERATIONS.SAVE, ProjectController.save);
+
 module.exports = router;
