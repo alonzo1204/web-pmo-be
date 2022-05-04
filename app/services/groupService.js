e163eb4e8c66f2f45c634db7c4d8169e75507adf
time2w8 <emgore.1410@gmail.com>
2022-05-04 11:24:04 -0500

    add project assigned information on my group request

--- app/services/groupService.js
+++ app/services/groupService.js
@@ -61,18 +61,35 @@ exports.getgroup = function (code) {
                     console.log(result[0].id);
                     mysqlConnection.query({
                         sql: `select g.group_weighted_average,
-                         u1.firstname as 'alumno1.nombre',
-                         u1.lastname as 'alumno1.apellido',
-                         u1.code as 'alumno1.codigo',
-                         u1.weighted_average as 'alumno1.prom',
-                         u2.id as 'alumno2.id',
-                         u2.firstname as 'alumno2.nombre',
-                         u2.lastname as 'alumno2.apellido',
-                         u2.code as 'alumno2.codigo_2',
-                         u2.weighted_average as 'alumno2.prom_2'
+                        u1.firstname as 'alumno1.nombre',
+                        u1.lastname as 'alumno1.apellido',
+                        u1.code as 'alumno1.codigo',
+                        u1.weighted_average as 'alumno1.prom',
+                        u2.id as 'alumno2.id',
+                        u2.firstname as 'alumno2.nombre',
+                        u2.lastname as 'alumno2.apellido',
+                        u2.code as 'alumno2.codigo_2',
+                        u2.weighted_average as 'alumno2.prom_2',
+
+                        p.id as 'project_assigned.id', 
+                        p.code as 'project_assigned.code',  
+                        p.name as 'project_assigned.name', 
+                        p.description as 'project_assigned.description', 
+                        p.general_objective as 'project_assigned.general_objective',
+                        
+                        comp.id as 'project_assigned.company.id',
+                        comp.name as 'project_assigned.company.name',
+                        comp.image as 'project_assigned.company.image',
+
+                        ca.id as 'project_assigned.career.id',
+                        ca.name as 'project_assigned.career.name'
+
                         from db_pmo_dev.group g 
                         left join user u1 on u1.id = g.student_1_id
                         left join user u2 on u2.id = g.student_2_id
+                        left join project p on p.id = g.project_assigned
+                        left join company comp on comp.id = p.company_id
+                        left join career ca on ca.id = p.career_id
                         where g.id = ${result[0].id}
                         group by u2.code and u1.code`,
                     }, function (error, result, fields) {
